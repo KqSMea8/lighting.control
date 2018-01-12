56630931bfe3f96e7b0011ff439eb3e7199fc583
rongfu <rongfu.leng@pohoocredit.com>
2018-01-12 19:31:21 +0800

    分项目

---
+++ backend/src/main/java/com/dikong/lightcontroller/config/BeanConfig.java
@@ -0,0 +1,18 @@
+package com.dikong.lightcontroller.config;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * Description
+ * </p>
+ * <p>
+ *
+ * @author lengrongfu
+ * @create 2018年01月12日上午8:03
+ * @see </P>
+ */
+@Component
+public class BeanConfig {
+
+}
