8386c00ed6d4ca56dea3cd997eb63b5f8cbcfd80
rongfu <rongfu.leng@pohoocredit.com>
2018-02-06 11:07:49 +0800

    添加tokne

--- backend/src/main/java/com/dikong/lightcontroller/config/Swagger2Config.java
+++ backend/src/main/java/com/dikong/lightcontroller/config/Swagger2Config.java
@@ -6,13 +6,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -36,8 +42,18 @@ public class Swagger2Config {
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
                 .apis(RequestHandlerSelectors.basePackage("com.dikong.lightcontroller.controller"))
-                .paths(PathSelectors.any()).build();
+                .paths(PathSelectors.any()).build().globalOperationParameters(setHeaderToken());
     }
+
+    private List<Parameter> setHeaderToken() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name("token").description("token").modelRef(new ModelRef("string"))
+                .parameterType("header").required(true).build();
+        pars.add(tokenPar.build());
+        return pars;
+    }
+
 
     private ApiInfo apiInfo() {
         String host = environment.getProperty("server.address", "127.0.0.1");
